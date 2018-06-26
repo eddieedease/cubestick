1ca5afc6ce9f759e0bed24b4f37d96d494a93a2e
eddieedease <eddie@edease.nl>
2018-06-26 12:37:42 +0200

    set up angular + bootstrap + jquery + main stylings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  PageNotFoundComponent
+} from './page-not-found/page-not-found.component';
+import {
+  SiteComponent
+} from './site/site.component';
+import {
+  BrowserAnimationsModule
+} from '@angular/platform-browser/animations';
+
+import {
+  HttpModule,
+  JsonpModule
+} from '@angular/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {
+  FormsModule
+} from '@angular/forms';
+
+import {
+  SerserService
+} from './serser.service';
+
+
+const appRoutes: Routes = [
+  { path: 'site', component:   SiteComponent
+},
+  { path: '',
+    redirectTo: '/site',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    HttpModule,
+    JsonpModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [SerserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
